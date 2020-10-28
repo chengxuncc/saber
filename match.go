b74d8ca1e95f619b47806aae4e708a5a96883d41
chengxuncc <chengxuncc@gmail.com>
2020-10-28 14:39:08 +0800

    feat: regular expression for matching and replacing

--- match.go
+++ match.go
@@ -3,6 +3,7 @@ package saber
 import (
 	"bufio"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -22,6 +23,44 @@ func (c *Compound) Grep(sub string) *Compound {
 				_, err := fmt.Fprintln(c.Stdout, line)
 				if err != nil {
 					return err
+				}
+			}
+		}
+		return nil
+	})
+}
+
+func Regex(expr string, replace ...string) *Compound {
+	return Do().Regex(expr, replace...)
+}
+
+func (c *Compound) Regex(expr string, replace ...string) *Compound {
+	var repl string
+	if len(replace) > 0 {
+		repl = replace[0]
+	}
+	return c.Call(func(c *Command) error {
+		if c.Stdin == nil {
+			return nil
+		}
+		r, err := regexp.Compile(expr)
+		if err != nil {
+			return err
+		}
+		scanner := bufio.NewScanner(c.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if repl != "" {
+				_, err := fmt.Fprintln(c.Stdout, r.ReplaceAllString(line, repl))
+				if err != nil {
+					return err
+				}
+			} else {
+				if r.MatchString(line) {
+					_, err := fmt.Fprintln(c.Stdout, line)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
